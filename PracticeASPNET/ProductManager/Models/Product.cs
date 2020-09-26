3853f82506f8a1067b789c3a5c511ce0499d364f
huy96cpqnvn <huy96cpqnvn@gmai.com>
2020-09-26 09:04:55 +0700

    Practice ASP.NET MVC

---
+++ PracticeASPNET/ProductManager/Models/Product.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text.Json;
+using System.Text.Json.Serialization;
+using System.Threading.Tasks;
+
+namespace ProductManager.Models
+{
+    public class Product
+    {
+        public string Id { get; set; }
+
+        [JsonPropertyName("img")] /*Khi muốn chuyển sang ánh xạ kiểu khác theo project của mình, chỉ về thư mục gốc */
+        public string Image { get; set; }
+
+        public string Name { get; set; }
+
+        public float Price { get; set; }
+
+        public override string ToString() => JsonSerializer.Serialize<Product>(this);
+    }
+}
